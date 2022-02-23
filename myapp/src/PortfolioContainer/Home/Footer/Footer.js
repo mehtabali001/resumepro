4ac255b2836229dfdc98d29749cf8254adacbf5b
mehtab ali <mehtabali001@gmail.com>
2022-02-23 21:53:11 +0500

    first commit

---
+++ myapp/src/PortfolioContainer/Home/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer(){
+    return (
+        <div className="footer-container">
+        <div className="footer-parent">
+            {/* <img 
+             src={require("C://Users/Mehtab Ali/Desktop/react_series/mktechsol/myapp/src/Materials/shape-bg.png")} alt="Some thing wrong" 
+             /> */}
+        </div>
+        </div>
+
+    );
+}
